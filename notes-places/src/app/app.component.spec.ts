b597c549da19406d4d326802531e68f8c9982ea7
NouhaDev <nouha.ghribi@gmail.com>
2019-07-09 11:17:09 +0200

    add file places

--- notes-places/src/app/app.component.spec.ts
+++ notes-places/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'notes-app'`, () => {
+  it(`should have as title 'notes-places'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('notes-app');
+    expect(app.title).toEqual('notes-places');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to notes-app!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to notes-places!');
   });
 });
